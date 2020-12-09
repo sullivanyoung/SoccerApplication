c8a1213f1dd10f16a13304785c09d0a8cbe464cb
Sullivan Young <sullivangyoung@gmail.com>
2020-12-09 17:22:13 -0500

    fixing ball/cpu movement issues

--- SoccerGame/Soccer.cs
+++ SoccerGame/Soccer.cs
@@ -77,64 +77,13 @@ namespace SoccerGame
 
             cpu.Top += speed;
 
-            //cpu functionality
-
-            if (userScore < 2)
-            {
-                if (cpu.Top < 203 || cpu.Top > 325)
-                {
-                    speed = -speed;
-                }
-            }
-            else
-            {
-                cpu.Top = ball.Top + 100;
-            }
+            //cpu functionality -- fix
 
 
-            //ball functionality
-            if(ball.Left < 75 && ball.Top > 275 && ball.Top < 320)
-            {
-                ball.Left = 529;
-                player.Left = 127;
-                cpu.Left = 928;
-                cpu.Top = 298;
-                ballx = -ballx;
-                ballx = 10;
-                cpuScore++;
-            }
 
-            if (ball.Left > 980 && ball.Top > 275 && ball.Top < 320)
-            {
-                ball.Left = 529;
-                player.Left = 127;
-                cpu.Left = 928;
-                ballx = +ballx;
-                ballx = 10;
-                userScore++;
-            }
+            //ball functionality -- fix
+   
 
-            if (ball.Top < 55 || ball.Top + ball.Height > 75)
-            {
-                bally = -bally;
-            }
-
-            if (ball.Left < 74 || ball.Left > 981)
-            {
-                ballx = -ballx;
-            }
-
-            if (ball.Bounds.IntersectsWith(player.Bounds))
-            {
-                ballx = -ballx;
-                ballx -= 2;
-            }
-
-            if(ball.Bounds.IntersectsWith(cpu.Bounds))
-            {
-                ballx = -ballx;
-                ballx += 2;
-            }    
 
             //player functionality
             if (moveUp == true && player.Top > 54)
